6aaa34dc44cdd47fd8934c5858202d1d02eaa216
Elvis Serafim <elvis.serafim@deliverit.com.br>
2024-01-26 15:32:04 -0300

    feat: update tests

--- src/__tests__/index.tsx
+++ src/__tests__/index.tsx
@@ -1,30 +1,3 @@
 import { act, render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import App from "@/app/page";
 import { ToastProvider } from "@/contexts/ToastContext";
-
-describe("App component", () => {
-  test("renders App component", async () => {
-    act(() => {
-      render(
-        <ToastProvider>
-          <App />
-        </ToastProvider>
-      );
-    });
-
-    expect(screen.getByText("5 Day Forecast")).toBeInTheDocument();
-  });
-
-  test("checks loadUserLocationWeather function", async () => {
-    act(() => {
-      render(
-        <ToastProvider>
-          <App />
-        </ToastProvider>
-      );
-    });
-    await screen.findByText("5 Day Forecast");
-    expect(screen.getByText("5 Day Forecast")).toBeInTheDocument();
-  });
-});
