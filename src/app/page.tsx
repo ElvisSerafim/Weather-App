6524216dceb5124227da49729df5f1059dad47ee
Elvis Serafim <elvis.serafim@deliverit.com.br>
2024-01-26 11:21:33 -0300

    feat: create main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,139 @@
-import Image from "next/image";
+"use client";
+import { useContext, useEffect, useState } from "react";
+import SwitchTemperatureScale from "@/components/SwitchTemperatureScale";
+import MainCard from "@/components/MainCard";
+import ForecastCard from "@/components/ForecastCard";
+import weatherService from "@/services/weather.service";
+import {
+  City,
+  CurrentWeather,
+  GetForecastWeather,
+  ScaleTemperatureEnum,
+} from "@/interfaces/weather.interface";
+import AutoCompleteSearch from "@/components/AutoCompleteSearch";
+import { getUserLocation } from "@/services/location.service";
+import { useToast } from "@/contexts/ToastContext";
 
-export default function Home() {
+export default function App() {
+  const { addToast } = useToast();
+  const DAYS_FORECAST = 6;
+  const [isSelectedCelsius, setIsSelectedCelsius] = useState<boolean>(true);
+  const [currentWeather, setCurrentWeather] = useState<CurrentWeather>();
+  const [currentCity, setCurrentCity] = useState<City | undefined>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const getCurrentWeather = async () => {
+    setIsLoading(true);
+    try {
+      const forecastRequest: GetForecastWeather = {
+        city: currentCity!.name,
+        days: DAYS_FORECAST,
+      };
+
+      const weatherData = await weatherService.getWeather(forecastRequest);
+      if (weatherData) {
+        setCurrentWeather(weatherData);
+      }
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      addToast(
+        "Error loading weather city!",
+        "Check your connection and try again.",
+        "error"
+      );
+    }
+  };
+
+  const isNightTime = () => {
+    if (currentWeather) {
+      const currentLocationHour = new Date(
+        currentWeather.location.localtime
+      ).getHours();
+
+      return currentLocationHour >= 17 || currentLocationHour < 5;
+    }
+  };
+
+  const loadUserLocationWeather = async () => {
+    try {
+      setIsLoading(true);
+      const weatherData = await getUserLocation();
+      setCurrentWeather(weatherData);
+      setIsLoading(false);
+    } catch (error) {
+      addToast(
+        "Error on load user location weather!",
+        "Check your location permissions.",
+        "error"
+      );
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (currentCity) {
+      getCurrentWeather();
+      isNightTime();
+    }
+  }, [currentCity]);
+
+  useEffect(() => {
+    loadUserLocationWeather();
+    isNightTime();
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div
+      className={`flex flex-col p-6 items-center w-100 min-h-screen ${
+        !isNightTime()
+          ? "bg-gradient-to-tr from-pink-500 to-yellow-500"
+          : "bg-gradient-to-tr from-blue-800 to-gray-900"
+      } text-white shadow-lg`}
+    >
+      <div className="flex flex-col w-full md:w-5/6 gap-10">
+        <div className="flex flex-col items-start justify-center gap-4 w-full">
+          <div className="flex flex-row w-full justify-end">
+            <SwitchTemperatureScale
+              isSelectedCelsius={isSelectedCelsius}
+              setIsSelectedCelsius={setIsSelectedCelsius}
             />
-          </a>
+          </div>
+          <div className="w-full md:w-full">
+            <AutoCompleteSearch setSelectedCity={setCurrentCity} />
+          </div>
+        </div>
+        <div className="w-full flex flex-col md:flex-row p-0 m-0">
+          <MainCard
+            currentWeather={currentWeather}
+            isLoading={isLoading}
+            scaleTemperature={
+              isSelectedCelsius
+                ? ScaleTemperatureEnum.Celsius
+                : ScaleTemperatureEnum.Fahrenheit
+            }
+          />
+        </div>
+        <div className="w-full">
+          <h1 className="text-2xl md:text-4xl font-bold text-left">
+            5 Day Forecast
+          </h1>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 xl:grid-cols-5 gap-4">
+          {currentWeather?.forecast.forecastday.slice(1).map((forecastData) => (
+            <ForecastCard
+              key={forecastData.date}
+              forecast={forecastData}
+              isLoading={isLoading}
+              scaleTemperature={
+                isSelectedCelsius
+                  ? ScaleTemperatureEnum.Celsius
+                  : ScaleTemperatureEnum.Fahrenheit
+              }
+            />
+          ))}
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
