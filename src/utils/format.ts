8dde35327df250e8672f8e3c6b25130ce31b7916
Elvis Serafim <elvis.serafim@deliverit.com.br>
2024-01-26 15:57:11 -0300

    feat: add isNightTime to format

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -5,6 +5,10 @@ const format = {
   descriptionDate: (dateToFormat: string) =>
     moment(dateToFormat).format("DD MMM, YYYY"),
   dayOfWeek: (dateToFormat: string) => moment(dateToFormat).format("dddd"),
+  isNightTime: (localtime: string) => {
+    const currentLocationHour = new Date(localtime).getHours();
+    return currentLocationHour >= 17 || currentLocationHour < 5;
+  },
 };
 
 export default format;
